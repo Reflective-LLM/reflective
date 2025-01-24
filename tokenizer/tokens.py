4f78557edf2edbe8a57cd2f2c490ed72b96529db
Zffu <radi0o0@outlook.com>
2025-01-24 15:26:27 +0100

    feat: removed dummy tokenizer func

--- tokenizer/tokens.py
+++ tokenizer/tokens.py
@@ -32,30 +32,3 @@ class TokenHolder:
 
         return tokens
         
-
-def tokenize(holder: TokenHolder, text: str) -> list[Token]:
-    tokens: list[Token] = []
-    splits: list[str] = text.split(" ")
-
-    for word in splits:
-        buff: str = ""
-        pos: int = 0
-
-        while pos < len(word):
-            found = False
-            for entry in holder.tokens:
-                if word[pos:] == entry and len(word[pos:]) > len(buff):
-                    buff = word[pos:]
-                    pos += 1
-                    found = True
-                    break
-
-            if not found:
-                pos += 1
-        
-        
-        if not buff == "":
-            tokens.append(Token(holder.tokens[buff], buff))
-            print(f"Token detected: id: {str(holder.tokens[buff])} with raw {buff}")
-
-    return tokens
