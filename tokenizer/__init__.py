1e96145a8a395ab1d1da3f796e58cb801e5b61f5
Zffu <radi0o0@outlook.com>
2025-01-24 15:25:56 +0100

    feat: made tokenize a member of TokenHolder

--- tokenizer/__init__.py
+++ tokenizer/__init__.py
@@ -4,31 +4,5 @@ Main file of the tokenizer
 
 from .tokens import TokenHolder, Token
 
-def tokenize(holder: TokenHolder, text: str) -> list[Token]:
-    tokens: list[Token] = []
-    splits: list[str] = text.split(" ")
-
-    for word in splits:
-        buff: str = ""
-        pos: int = 0
-
-        while pos < len(word):
-            found = False
-            for entry in holder.tokens:
-                if word[pos:] == entry and len(word[pos:]) > len(buff):
-                    buff = word[pos:]
-                    pos += 1
-                    found = True
-                    break
-
-            if not found:
-                pos += 1
-        
-        
-        if not buff == "":
-            tokens.append(Token(holder.tokens[buff], buff))
-            print(f"Token detected: id: {str(holder.tokens[buff])} with raw {buff}")
-
-    return tokens
 
 from .reader import readTokenFile
--- tokenizer/tokens.py
+++ tokenizer/tokens.py
@@ -6,3 +6,56 @@ class Token:
 class TokenHolder:
     def __init__(self):
         self.tokens: dict(str, int) = {}
+    
+    def tokenize(text: str) -> list[Token]:
+        tokens: list[Token] = []
+        splits: list[str] = text.split(" ")
+        for word in splits:
+            buff: str = ""
+            pos: int = 0
+
+            while pos < len(word):
+                found = False
+                for entry in self.tokens:
+                    if word[pos:] == entry and len(word[pos:]) > len(buff):
+                        buff = word[pos:]
+                        pos += 1
+                        found = True
+                        break
+
+                if not found:
+                    pos += 1
+            
+            
+            if not buff == "":
+                tokens.append(Token(self.tokens[buff], buff))
+
+        return tokens
+        
+
+def tokenize(holder: TokenHolder, text: str) -> list[Token]:
+    tokens: list[Token] = []
+    splits: list[str] = text.split(" ")
+
+    for word in splits:
+        buff: str = ""
+        pos: int = 0
+
+        while pos < len(word):
+            found = False
+            for entry in holder.tokens:
+                if word[pos:] == entry and len(word[pos:]) > len(buff):
+                    buff = word[pos:]
+                    pos += 1
+                    found = True
+                    break
+
+            if not found:
+                pos += 1
+        
+        
+        if not buff == "":
+            tokens.append(Token(holder.tokens[buff], buff))
+            print(f"Token detected: id: {str(holder.tokens[buff])} with raw {buff}")
+
+    return tokens
